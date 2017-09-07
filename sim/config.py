ee00eb9b2c0cf424ab85ebf69509900aa4ec4c62
Sean R. Lang <srlang@ncsu.edu>
2017-09-07 16:54:14 +0300

    configuration options and utilitary functions defined

---
+++ sim/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Sean R. Lang <sean.lang@cs.helsinki.fi>
+
+'''
+Configuration options for the code for quick tuning and customizations.
+'''
+
+DEBUG = True
---
+++ sim/utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Sean R. Lang <sean.lang@cs.helsinki.fi>
+
+'''
+Utilitary functions that may be of use elsewhere across the application but
+don't themselves belong in any specific location.
+'''
+
+from config import DEBUG
+
+def PD(msg, method=None):
+    if DEBUG:
+        if method:
+            print("%s: %s" % (method, msg))
+        else:
+            print(msg)
