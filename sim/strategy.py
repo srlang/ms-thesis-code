b81213103f0a75dec3d2da2a3265c6ee007b86b7
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-09-18 15:32:45 +0300

    commentary to keep track of things and minor adjustments

--- sim/strategy.py
+++ sim/strategy.py
@@ -1,10 +1,15 @@
-from itertools import combinations
+from itertools  import combinations
+from statistics import median
 
-#from .. import cribbage
-from cribbage import score_hand
+from cribbage   import score_hand
+from utils      import PD
 
 KEEP_NUMBER = 4
 
+######
+# remnant of a time in which I thought of keeping each strategy in a separate
+# file and import dynamically
+# As these will likely not get used, their tests are just as useless
 def _strategy_interface(cards):
     return cards[:KEEP_NUMBER]
 
@@ -12,9 +17,11 @@ def _get_strategy_interface(module, strategy_str):
     # return the method object to call corresponding to the strategy named
     # in the given string
     return getattr(module, strategy_str)
+######
 
 def possible_keep_throw_choices(cards):
     # possible choices to keep and throw so that we know what
+    # Returns: list of tuples ([cards_in_hand], [cards_thrown_to_crib])
     # visual inspection: good
     # tested for basic properties
     combos = combinations(cards, KEEP_NUMBER)
@@ -25,33 +32,53 @@ def possible_keep_throw_choices(cards):
     return ret
 
 def _enumerate_possible_hand_values(keep, toss):
+    # Enumerate possible hand values knowing which cards are kept and tossed
+    # Returns: list [int] of hand scores
+    #   It is important to the implementation of this project that none of
+    #   the cause for each of the individual values is not needed.
+    #   i.e. we only care about the aggregate list of scores, not the card
+    #   combinations that caused each individual score.
+    # tested
     possible_cut_cards = [x for x in range(52)
             if x not in toss and x not in keep]
     possible_hand_values = []
     for cut_card in possible_cut_cards:
-        possible_hand_values.append(score_hand(hand, cut_card))
+        possible_hand_values.append(score_hand(keep, cut_card))
     return possible_hand_values
 
 def hand_compute_value(cards, list_func):
+    # Compute a function over the list of possible scores available to each
+    #   keep,toss combination
+    # Return dict{tuple(tuple(keep),tuple(toss))
+    #               --> list_func(possible_hand_values(keep,toss))}
+    # tested
+    PD('entering', 'hand_compute_value')
     values = {}
     for kt in possible_keep_throw_choices(cards):
+        PD('>> loop: kt=%s' % str(kt))
         k,t = kt
         hv = _enumerate_possible_hand_values(k,t)
-        values[kt] = list_func(hv)
+        values[(tuple(k),tuple(t))] = list_func(hv)
+    PD('exiting', 'hand_compute_value')
     return values
 
 def select_min_valued_hand(possible_hands_values_dict):
+    # Find the hand combination that returns the minimum associated score
     return _select_valued_hand(possible_hands_values_dict, min)
 
 def select_max_valued_hand(possible_hands_values_dict):
+    # Find the hand combination that returns the maximum associated score
     # select the key corresponding to the maximum value
     return _select_valued_hand(possible_hands_values_dict, max)
 
 def _select_valued_hand(possible_hands_values_dict, find_fn):
+    # Find the hand that returns the value determined through find_fn
+    # Returns: FIRST (keep,toss) that gives the value returned by find_fn
+    #           applied to the values of the input dict
     # reference: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
     # make them lists to be able to index later
-    values = list(possible_hand_values_dict.values())
-    keys = list(possible_hand_values_dict.keys())
+    values = list(possible_hands_values_dict.values())
+    keys = list(possible_hands_values_dict.keys())
     return keys[values.index(find_fn(values))]
 
 
@@ -64,24 +91,27 @@ def list_max(values):
 def list_avg(values):
     return sum(values) / len(values)
 
+def list_med(values):
+    return median(values)
+
 # TODO: in the future, find a way to use pre-computed/stored values
 def hand_max_min(cards):
     # Safe strategy, pick the hand that has the most guaranteed points
-    return select_max_valued_hand(hand_compute_values(cards, list_min))
+    return select_max_valued_hand(hand_compute_value(cards, list_min))
 
 def hand_max_avg(cards):
     # Choose the hand that has the maximum average points when accounting for
     # cut card possibilities
-    return select_max_valued_hand(hand_compute_values(cards, list_avg))
+    return select_max_valued_hand(hand_compute_value(cards, list_avg))
 
 def hand_max_med(cards):
     # Choose the hand that has the maximum median points when accounting for
     # cut card possibilities
-    return select_max_valued_hand(hand_compute_values(cards, list_med))
+    return select_max_valued_hand(hand_compute_value(cards, list_med))
 
 def hand_max_poss(cards):
     # Hail Mary strategy, pick the hand that has the chance of the largest value.
-    return select_max_valued_hand(hand_compute_values(cards, list_max))
+    return select_max_valued_hand(hand_compute_value(cards, list_max))
 
 
 def pegging_max_avg(cards):
