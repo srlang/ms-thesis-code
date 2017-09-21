10be1e126c6f69e5210841f6b21be85ca5f0bae4
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-09-21 12:54:58 +0300

    config: section for database added

--- sim/config.py
+++ sim/config.py
@@ -6,4 +6,11 @@
 Configuration options for the code for quick tuning and customizations.
 '''
 
-DEBUG = False #True
+DEBUG = True
+
+DB_ENGINE = 'sqlite:///:memory:'
+DB_ECHO = True
+
+DB_POPULATE_SAVE_INTERVAL = 100
+DB_UPDATE_SAVE_INTERVAL_AGGPLAYEDHAND = 50
+DB_UPDATE_SAVE_INTERVAL_PLAYED_HAND = 50
