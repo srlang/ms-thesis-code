c5ce069e46ff3cb7072c2441ebacf054950a5f9b
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-10-19 20:29:09 +0300

    basic progress for hand picking strategies, using db

--- sim/strategy2.py
+++ sim/strategy2.py
@@ -1,6 +1,14 @@
 # Sean R. Lang <sean.lang@cs.helsinki.fi>
 
-from cribbage import score_hand
+from itertools  import      combinations
+
+from cribbage   import      score_hand
+
+from records    import      engine,\
+                            session,\
+                            PlayedHandRecord,\
+                            KeepThrowStatistics
+                    
 
 from utils import PD
 
@@ -26,52 +34,89 @@ def _possible_keep_toss_tuple_list(cards):
         ret.append((tuple(combo), tuple(cards_not_in_combo)))
     return ret
 
-def _retrieve_hand_statistics(kt_tuple):
+def _retrieve_hand_statistics(kt_tuple, session=session):
     k,t = kt_tuple
-    # TODO
-    pass
+    q = session.query(KeepThrowStatistics).filter_by(\
+            kcard0=k[0],\
+            kcard1=k[1],\
+            kcard2=k[2],\
+            kcard3=k[3],\
+            tcard0=t[0],\
+            tcard1=t[1])
+    return q.one_or_none()
+
+def possible_KeepThrowStatistics(cards):
+    ret = [_retrieve_hand_statistics(kt_t)\
+            for kt_t in possible_keep_toss_tuple_list(cards)]
+    return [x for x in ret if x is not None]
+
+def _retrieve_property_list(list_kts, prop_name):
+    return [kts.__dict__[prop_name] for kts in list_kts]
+
+def _get_all_indices(list_vals, spec_val):
+    return [i for i,x in enumerate(list_vals) if x == spec_val]
+
+def _get_by_multiple_indices(list_vals, indices):
+    return [list_vals[i] for i in indices]
+
+def _to_keep_toss_tuple(kts):
+    return ((kts.kcard0, kts.kcard1, kts.kcard2, kts.kcard3), (kts.tcard0, kts.tcard1))
+
+def hand_picker(cards, hand_property, eval_fn):
+    kts = possible_KeepThrowStatistics(cards)
+    props = _retrieve_property_list(kts, hand_property)
+    _prop = eval_fn(props)
+    p_indices = _get_all_indices(props, _prop)
+    kts_ret = _get_by_multiple_indices(kts, p_indices)
+    return [_to_keep_toss_tuple(x) for x in kts_ret]
 
 def hand_max_min(card):
     # Choose the hand(s) with the maximum minimum.
     # i.e. most guaranteed points
-    # TODO
-    pass
+    return hand_picker(cards, 'kmin', max)
 
 def hand_max_avg(cards):
     # Choose the hand(s) with the maximum average score
-    # TODO
-    pass
+    return hand_picker(cards, 'kavg', max)
 
 def hand_max_med(cards):
     # Choose the hand(s) with the maximum median score
-    # TODO
-    pass
+    return hand_picker(cards, 'kmed', max)
 
 def hand_max_poss(cards):
     # Choose the hand(s) with the maximum possible score (gambler's strategy)
-    # TODO
-    pass
+    return hand_picker(cards, 'kmin', max)
 
 def hand_min_avg_crib(cards):
     # Choose the hand(s) with the minimum average crib
-    # TODO
-    pass
-
-def _stanley(cards):
-    return hand_min_avg(cards)
+    return hand_picker(cards, 'tavg', min)
 
 def hand_max_avg_both(cards):
     # Choose the hand(s) with the maximum sum of average crib + average hand
+    kts = possible_KeepThrowStatistics(cards)
+    kavgs = _retrieve_property_list(kts, 'kavg')
+    tavgs = _retrieve_property_list(kts, 'tavg')
+    avgs = [kavgs[i] + tavgs[i] for i in xrange(len(kavgs))]
+    max_avg = max(avgs)
+    p_indcs = _get_all_indices(avgs, max_avg)
+    kts_ret = _get_by_multiple_indices(kts, p_indcs)
+    return [_to_keep_toss_tuple(x) for x in kts_ret]
+
+def pegging_max_avg_gained(cards):
+    # Choose the hand which has gained the maximum average of points for the
+    # player. This would typically be useful as a Hail Mary for the dealer in
+    # a very close game at the last hand.
     # TODO
     pass
 
-def pegging_max_avg(cards):
-    # Choose the hand which has the maximum average pegging points scored
-    # TODO
-    pass
-
-def pegging_max_med(cards):
+def pegging_max_med_gained(cards):
     # Choose the hand which has the maximum median pegging points scored
     # TODO
     pass
+
+def pegging_min_avg_given(cards):
+    # Choose the hand which has given the least points up, on average
+    # TODO
+    pass
+
 
---
+++ sim/test_strategy2.py
@@ -0,0 +1,131 @@
+# Sean R. Lang
+
+from strategy2 import   *
+from strategy2 import   _possible_keep_toss_tuple_list,\
+                        _retrieve_hand_statistics,\
+                        _retrieve_property_list,\
+                        _get_all_indices,\
+                        _get_by_multiple_indices,\
+                        _to_keep_toss_tuple
+
+
+def test__possible_keep_toss_tuple_list():
+    cards = [0, 1, 2, 3, 4, 5]
+    combos = [
+                ((0, 1, 2, 3), (4, 5)),
+                ((0, 1, 2, 4), (3, 5)),
+                ((0, 1, 2, 5), (3, 4)),
+                ((0, 1, 3, 4), (2, 5)),
+                ((0, 1, 3, 5), (2, 4)),
+                ((0, 1, 4, 5), (2, 3)),
+                ((0, 2, 3, 4), (1, 5)),
+                ((0, 2, 3, 5), (1, 4)),
+                ((0, 2, 4, 5), (1, 3)),
+                ((0, 3, 4, 5), (1, 2)),
+                ((1, 2, 3, 4), (0, 5)),
+                ((1, 2, 3, 5), (0, 4)),
+                ((1, 2, 4, 5), (0, 3)),
+                ((1, 3, 4, 5), (0, 2)),
+                ((2, 3, 4, 5), (0, 1))
+                ]
+    poss_kt_tl = _possible_keep_toss_tuple_list(cards)
+    for kt in poss_kt_tl:
+        assert kt in combos
+    for combo in combos:
+        assert combo in poss_kt_tl
+
+def test__retrieve_hand_statistics():
+    keep = (0, 1, 2, 3)
+    toss = (4, 5)
+    kts = _retrieve_hand_statistics((keep,toss)) #, session=session)
+    assert kts is not None
+    assert kts.kcard0 == 0
+    assert kts.kcard1 == 1
+    assert kts.kcard2 == 2
+    assert kts.kcard3 == 3
+    assert kts.tcard0 == 4
+    assert kts.tcard1 == 5
+
+def test_possible_KeepThrowStatistics():
+    cards = [0, 1, 2, 3, 4, 5]
+    pass
+
+def test__retrieve_property_list():
+    kts = [
+            KeepThrowStatistics(kcard0=12, kcard1=23, kcard2=33, kcard3=40), #, tcard0=2, tcard1=3)
+            KeepThrowStatistics(kcard0=11, kcard1=22, kcard2=33, kcard3=44),
+            KeepThrowStatistics(kcard0=12, kcard1=23, kcard2=34, kcard3=45),
+            ]
+    props = _retrieve_property_list(kts, 'kcard1')
+    assert props == [23, 22, 23]
+
+def test__get_all_indices():
+    lst = [1, 1, 2, 3, 5, 8]
+    assert _get_all_indices(lst, 1) == [0, 1]
+    assert _get_all_indices(lst, 2) == [2]
+    lst = [1, 1, 1, 1, 1, 2]
+    assert _get_all_indices(lst, 1) == [0, 1, 2, 3, 4]
+    lst = [1, 1, 1, 2, 1, 2]
+    assert _get_all_indices(lst, 1) == [0, 1, 2, 4]
+
+def test__get_by_multiple_indices():
+    tlist = range(10)
+    assert _get_by_multiple_indices(tlist, [0, 1, 3]) == [0, 1, 3]
+    assert _get_by_multiple_indices(tlist, [0, 3, 5]) == [0, 3, 5]
+    assert _get_by_multiple_indices(tlist, [0, 2, 8]) == [0, 2, 8]
+    assert _get_by_multiple_indices(tlist, [0, 8, 3]) == [0, 8, 3]
+
+def  test__to_keep_toss_tuple():
+    kts = KeepThrowStatistics(kcard0=1,
+            kcard1=2,
+            kcard2=3,
+            kcard3=4,
+            tcard0=0,
+            tcard1=5,
+            kmin=0,
+            kmax=29,
+            kmed=22,
+            kavg=21,
+            kmod=12,
+            tmin=0,
+            tmax=29,
+            tmed=22,
+            tavg=21,
+            tmod=12)
+    assert _to_keep_toss_tuple(kts) == ((1,2,3,4),(0,5))
+
+def test_hand_picker():
+    assert False
+    pass
+
+def test_hand_max_min():
+    assert False
+    pass
+
+def test_hand_max_avg():
+    assert False
+    pass
+
+def test_hand_max_med():
+    assert False
+    pass
+
+def test_hand_max_poss():
+    assert False
+    pass
+
+def test_hand_min_avg_crib():
+    assert False
+    pass
+
+def test_hand_max_avg_both():
+    assert False
+    pass
+
+def test_pegging_max_avg():
+    assert False
+    pass
+
+def test_pegging_max_med():
+    assert False
+    pass
