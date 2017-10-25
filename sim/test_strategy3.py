61de234ae5048b471440bf6a76deab1751fe9591
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-10-26 02:01:49 +0300

    added test stubs

---
+++ sim/test_strategy3.py
@@ -0,0 +1,47 @@
+# Sean R. Lang <sean.lang@cs.helsinki.fi>
+
+from strategy3 import *
+from strategy3 import _retrieve_property_list
+
+def test__retrieve_property_list():
+    assert False
+
+def test_kts_evaluator():
+    assert False
+
+def test_pegging_evaluator():
+    assert False
+
+def test_possible_AggregatePlayedHandRecords():
+    assert False
+
+def test_hand_evaluator():
+    assert False
+
+def test_hand_max_min():
+    assert False
+
+def test_hand_max_avg():
+    assert False
+
+def test_hand_max_med():
+    assert False
+
+def test_hand_max_poss():
+    assert False
+
+def test_hand_max_min():
+    assert False
+
+def test_crib_min_avg():
+    assert False
+
+def test_pegging_max_avg_gained():
+    assert False
+
+def test_pegging_max_med_gained():
+    assert False
+
+def test_pegging_min_avg_given():
+    assert False
+
