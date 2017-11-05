014ed328589dae736c61e936ea7224186cb3bfac
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-11-05 17:34:21 +0200

    basic outline for analysis to produce nice pictures for the thesis later

---
+++ sim/analyze.py
@@ -0,0 +1,32 @@
+# Sean R. Lang <sean.lang@cs.helsinki.fi>
+
+'''
+Script to be used after a bunch of runs have been made,
+making some plots that could be useful for the thesis.
+
+Ideas:
+    plot a matrix of a single strategy in heatmap/grayscale with strength
+        - actually 2: 1 for the dealer, 1 for the pone
+    plot a time-stretch of matrices of a single strategy in grayscale/heatmap
+        as time progresses for
+
+This can be left for later
+'''
+
+def plot_single_strategy(file_name, strat_name):
+    # TODO
+    pass
+
+def plot_all_strategies(file_name):
+    # TODO
+    pass
+
+def plot_strategy_over_time(file_format, strat_name):
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    # do stuff
+    # [[None]*121]*121 --> does not work: all rows same pointer
+    pass
---
+++ sim/test_analyze.py
@@ -0,0 +1,10 @@
+# Sean R. Lang <sean.lang@cs.helsinki.fi>
+
+def test_plot_single_strategy():
+    assert False
+
+def test_plot_all_strategies():
+    assert False
+
+def test_plot_strategy_over_time():
+    assert False
