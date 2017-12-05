1ae949ae9e63cbae341185b369b5d24b6a8e8a4b
Sean R. Lang <sean.lang@cs.helsinki.fi>
2017-12-05 17:23:59 +0200

    backup ukko node scripts

---
+++ c/shellscripts/c_test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+WORK_DIR=~/c_ukko2
+CURR_DIR=$(pwd)
+#DB_DIR=/local/srlang
+DB_DIR=/tmp/srlang
+DB_BLANK="$WORK_DIR/tmp_db_c.db.empty"
+DB_FILE_TEMP="tmp_db_c_"
+DB_COUNT=64
+DB_SINGLE="tmp_db_c.db"
+DB_OUT_DIR="/home/srlang/db_out_dir"
+
+date
+
+echo "DB setup"
+test -d "$DB_DIR" || mkdir -v "$DB_DIR"
+cd "$DB_DIR"
+
+#for i in {0..$DB_COUNT}; do
+for ((i=0;i<$DB_COUNT;i+=1)); do
+	cp -v "$DB_BLANK" "$DB_FILE_TEMP$i.db"
+done
+
+###cp -v "$DB_BLANK" "$DB_SINGLE"
+
+
+
+echo "Running DB populator"
+
+cd "$WORK_DIR"
+time ./db_populate
+#gdb ./db_populate
+
+
+echo "DB cleanup"
+
+cd "$DB_DIR"
+
+for ((i=0;i<$DB_COUNT;i+=1)); do
+	test -f "$DB_FILE_TEMP$i.db" && cp -v "$DB_FILE_TEMP$i.db" "$DB_OUT_DIR/$DB_FILE_TEMP$i.db"
+	test -f "$DB_FILE_TEMP$i.db" && rm -v "$DB_FILE_TEMP$i.db"
+done
+
+###test -f "$DB_SINGLE" && cp -v "$DB_SINGLE" ~/
+###test -f "$DB_SINGLE" && rm -v "$DB_SINGLE"
+
+# 
+cd "$CURR_DIR"
+rmdir -v "$DB_DIR" || rm -rfv "$DB_DIR"
+
+date
